1a728f49098d5639648ede6209c75e0e79a39780
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-30 16:57:27 -0300

    05-Add and show players
    05-Add and show players

--- src/components/stage_one.jsx
+++ src/components/stage_one.jsx
@@ -1,8 +1,11 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import { Button, Form, Alert } from 'react-bootstrap';
+
+import { MyContext } from '../context';
 
 const Stage1 = () => {
     const textInput = useRef();
+    const context = useContext(MyContext);
 
 
     const handleSubmit = (e) => {
@@ -11,9 +14,12 @@ const Stage1 = () => {
 
         /// run validation
         /// add to list
-        console.log(value)
+        context.addPlayer(value);
         textInput.current.value = '';
     }
+
+
+    console.log(context)
 
     return(
         <>
@@ -32,6 +38,27 @@ const Stage1 = () => {
                 <Button className='miami' variant='primary' type='submit' >
                     Add player
                 </Button>
+                { context.players && context.players.length > 0 ?
+                <>
+                    <hr/>
+                    <div>
+                        <ul className='list-group'>
+                            { context.players.map((player,idx)=>(
+                                <li key={idx} className='list-group-item d-flex justify-content-between align-items-center list-group-item-action'>
+                                    {player}
+                                    <span
+                                        className='badge badge-danger'
+                                        onClick={()=> alert('delete')}
+                                    >
+                                        X
+                                    </span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </>
+                :null
+                }
             </Form>
         </>
     )
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -8,13 +8,23 @@ const MyProvider = (props) => {
     const [result,setResult] = useState('');
 
 
+    const addPlayerHandler = (name) =>{
+        setPlayers(prevState => ([
+            ...prevState,
+            name
+        ]));
+    }
+
+
+
     return(
         <MyContext.Provider value={{
             // STATE
             stage:stage,
             players:players,
-            result:result
+            result:result,
             // METHODS
+            addPlayer:addPlayerHandler
         }}>
             {props.children}
         </MyContext.Provider>
